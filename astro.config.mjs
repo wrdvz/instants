3a345afc3cec9c4a8be0a5d4c71ea1d16ba4c8f7
wrdvz <161055648+wrdvz@users.noreply.github.com>
2025-12-25 12:07:56 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-  output: "static"
+  output: "server",
+  adapter: cloudflare(),
 });
